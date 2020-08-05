fbbd06d0fb586c0176c9dd6a5cd7e781fb150d85
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 11:07:34 +0530

    updated apiOverview with api_urls

--- todos_drf/api/views.py
+++ todos_drf/api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+# Important rest_framework imports
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse('API BASE POINT', safe=False)
+
+    api_urls = {
+        'List':'/task-list/',
+        'Detail':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
