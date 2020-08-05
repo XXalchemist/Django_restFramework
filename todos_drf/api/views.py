a0085cd3a493e15405ae3b65e8ac41f948792c13
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 11:14:03 +0530

    added taskList api view

--- todos_drf/api/views.py
+++ todos_drf/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .serializers import TaskSerializer
+from .models import Task
 # Important rest_framework imports
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -18,3 +19,9 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
